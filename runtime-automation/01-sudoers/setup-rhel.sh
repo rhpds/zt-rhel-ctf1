dd2c447c76d6bf1611aa524cdb792670d5e52deb
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-09-25 11:23:32 -0400

    added setup and check scripting from instruqt

--- runtime-automation/01-sudoers/setup-rhel.sh
+++ runtime-automation/01-sudoers/setup-rhel.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo "Starting module called 01-sudoers" >> /tmp/progress.log
+
+useradd webteam
+echo redhat | passwd --stdin webteam
+dnf install -y httpd
+echo "webteam ALL=(ALL) NOPASSWD:systemctl httpd #THIS DOESN'T WORK -Scott" > /etc/sudoers.d/webteam-sudo
